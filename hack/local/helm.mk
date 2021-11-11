28175a831e795be128465bab9382587d468495e0
Dimitar Mirchev <dimitar.mirchev@sap.com>
2021-11-11 16:58:32 +0200

    Adds the ability to install the authenticator with cert-manager

--- hack/local/helm.mk
+++ hack/local/helm.mk
@@ -1,11 +1,15 @@
-HELM				?= helm3 # expected to be helm v3
-HELM_CHART_DIR		?= helm/oidc-webhook-authenticator
-HELM_NAMESPACE		?= oidc-webhook-authenticator-system
-HELM_RELEASE_NAME	?= oidc-wh
-HELM_OUTPUT_DIR		?= tmp
+# SPDX-FileCopyrightText: 2021 SAP SE or an SAP affiliate company and Gardener contributors
+#
+# SPDX-License-Identifier: Apache-2.0
+
+HELM				?= helm
+HELM_CHART_DIR		?= charts/oidc-webhook-authenticator
+HELM_NAMESPACE		?= oidc-webhook-authenticator
+HELM_RELEASE_NAME	?= oidc-webhook-authenticator
+HELM_OUTPUT_DIR		?= helm-templates
 
 helm-clean: ## Clean up templated helm chart
-	@rm -Rf $(HELM_OUTPUT_DIR)
+	@rm -rf $(HELM_OUTPUT_DIR)
 
 helm-lint: ## Run helm lint against helm chart.
 	@$(HELM) lint $(HELM_CHART_DIR)
@@ -16,10 +20,6 @@ helm-install: ## Run helm upgrade --install.
 helm-uninstall: ## Run helm uninstall.
 	$(HELM) uninstall --namespace $(HELM_NAMESPACE) $(HELM_RELEASE_NAME)
 
-helm-package: ## Run helm package
-	$(HELM) package --dependency-update $(HELM_CHART_DIR)
-
 helm-template: helm-clean ## Run helm template
 	@mkdir -p $(HELM_OUTPUT_DIR)
 	@$(HELM) template $(HELM_RELEASE_NAME) $(HELM_CHART_DIR) --namespace $(HELM_NAMESPACE) --output-dir $(HELM_OUTPUT_DIR)
-
